04a708011e1f5995516f57bc7d3c3611c9eb812c
Bryan Daniel <bryand@us.ibm.com>
2021-06-21 12:13:33 -0500

    initial vsi code

---
+++ vsi/variables.tf
@@ -0,0 +1,282 @@
+##############################################################################
+# Account Variables
+##############################################################################
+
+variable ibmcloud_api_key {
+  description = "The IBM Cloud platform API key needed to deploy IAM enabled resources"
+  type        = string
+  default     = ""
+}
+
+variable resource_group {
+  description = "Name of resource group to create the VSI"
+  type        = string
+  default     = "workday"
+}
+
+variable ibm_region {
+  description = "IBM Cloud region where all resources will be deployed"
+  type        = string
+  default     = "us-east"
+}
+
+variable tags {
+    description = "Enter any tags that you want to associate with VSI and associated resources."
+    type        = list(string)
+    default     = [
+      "project:gcatwd",
+      "env:dev"
+    ]
+}
+
+
+
+
+##############################################################################
+# Common VSI Variables
+##############################################################################
+
+variable vpc_name {
+  description = "Name of VPC where the VSI instance will be created"
+  type        = string
+  default     = "bd-wday-vpc"
+}
+
+variable ssh_key_name {
+  description = "Name of the ssh key to use for vsi."
+  type        = string
+  default     = "bryand-ssh-key"
+}
+
+
+##############################################################################
+# VSI 1 Variables
+##############################################################################
+
+variable vsi1_name {
+  description = "Name of the VSI instance to be created"
+  type        = string
+  default     = "wd-vsi-1"
+}
+
+variable vsi1_image_name {
+  description = "Image name used for VSI. Run 'ibmcloud is images' to find available images in a region"
+  type        = string
+  default     = "ibm-centos-8-3-minimal-amd64-3"
+}
+
+variable vsi1_machine_profile {
+  description = "VSI machine profile (i.e. mx2-32x256). Run 'ibmcloud is instance-profiles' to get a list of regional profiles"
+  type        =  string
+  default     = "mx2-32x256"
+}
+
+variable vsi1_vpc_zone {
+  description = "Name of the zone where the VSI will be deployed (i.e. us-south-1, us-south-2, us-east-1, etc). Zone should match VPC region."
+  type        = string
+  default     = "us-east-1"
+}
+
+variable vsi1_vpc_subnet_name {
+  description = "Name of the subnet where the VSI will be deployed. Subnet MUST be part of the VPC and zone or else VSI creation will fail."
+  type        = string
+  default     = "subnet-az1-priv"
+}
+
+##############################################################################
+
+
+
+##############################################################################
+# VSI 2 Variables
+##############################################################################
+
+variable vsi2_name {
+  description = "Name of the VSI instance to be created"
+  type        = string
+  default     = "wd-vsi-2"
+}
+
+variable vsi2_image_name {
+  description = "Image name used for VSI. Run 'ibmcloud is images' to find available images in a region"
+  type        = string
+  default     = "ibm-centos-8-3-minimal-amd64-3"
+}
+
+variable vsi2_machine_profile {
+  description = "VSI machine profile (i.e. mx2-32x256). Run 'ibmcloud is instance-profiles' to get a list of regional profiles"
+  type        =  string
+  default     = "mx2-32x256"
+}
+
+variable vsi2_vpc_zone {
+  description = "Name of the zone where the VSI will be deployed (i.e. us-south-1, us-south-2, us-east-1, etc). Zone should match VPC region."
+  type        = string
+  default     = "us-east-2"
+}
+
+variable vsi2_vpc_subnet_name {
+  description = "Name of the subnet where the VSI will be deployed. Subnet MUST be part of the VPC and zone or else VSI creation will fail."
+  type        = string
+  default     = "subnet-az2-priv"
+}
+
+##############################################################################
+
+
+
+##############################################################################
+# VSI 3 Variables
+##############################################################################
+
+variable vsi3_name {
+  description = "Name of the VSI instance to be created"
+  type        = string
+  default     = "wd-vsi-3"
+}
+
+variable vsi3_image_name {
+  description = "Image name used for VSI. Run 'ibmcloud is images' to find available images in a region"
+  type        = string
+  default     = "ibm-centos-8-3-minimal-amd64-3"
+}
+
+variable vsi3_machine_profile {
+  description = "VSI machine profile (i.e. mx2-32x256). Run 'ibmcloud is instance-profiles' to get a list of regional profiles"
+  type        =  string
+  default     = "mx2-32x256"
+}
+
+variable vsi3_vpc_zone {
+  description = "Name of the zone where the VSI will be deployed (i.e. us-south-1, us-south-2, us-east-1, etc). Zone should match VPC region."
+  type        = string
+  default     = "us-east-3"
+}
+
+variable vsi3_vpc_subnet_name {
+  description = "Name of the subnet where the VSI will be deployed. Subnet MUST be part of the VPC and zone or else VSI creation will fail."
+  type        = string
+  default     = "subnet-az3-priv"
+}
+
+##############################################################################
+
+
+##############################################################################
+# VSI 4 Variables
+##############################################################################
+
+variable vsi4_name {
+  description = "Name of the VSI instance to be created"
+  type        = string
+  default     = "scylla-vsi-1"
+}
+
+variable vsi4_image_name {
+  description = "Image name used for VSI. Run 'ibmcloud is images' to find available images in a region"
+  type        = string
+  default     = "ibm-centos-8-3-minimal-amd64-3"
+}
+
+variable vsi4_machine_profile {
+  description = "VSI machine profile (i.e. mx2-32x256). Run 'ibmcloud is instance-profiles' to get a list of regional profiles"
+  type        =  string
+  default     = "mx2-32x256"
+}
+
+variable vsi4_vpc_zone {
+  description = "Name of the zone where the VSI will be deployed (i.e. us-south-1, us-south-2, us-east-1, etc). Zone should match VPC region."
+  type        = string
+  default     = "us-east-1"
+}
+
+variable vsi4_vpc_subnet_name {
+  description = "Name of the subnet where the VSI will be deployed. Subnet MUST be part of the VPC and zone or else VSI creation will fail."
+  type        = string
+  default     = "subnet-az1-priv"
+}
+
+##############################################################################
+
+
+##############################################################################
+# VSI 5 Variables
+##############################################################################
+
+variable vsi5_name {
+  description = "Name of the VSI instance to be created"
+  type        = string
+  default     = "scylla-vsi-2"
+}
+
+variable vsi5_image_name {
+  description = "Image name used for VSI. Run 'ibmcloud is images' to find available images in a region"
+  type        = string
+  default     = "ibm-centos-8-3-minimal-amd64-3"
+}
+
+variable vsi5_machine_profile {
+  description = "VSI machine profile (i.e. mx2-32x256). Run 'ibmcloud is instance-profiles' to get a list of regional profiles"
+  type        =  string
+  default     = "mx2-32x256"
+}
+
+variable vsi5_vpc_zone {
+  description = "Name of the zone where the VSI will be deployed (i.e. us-south-1, us-south-2, us-east-1, etc). Zone should match VPC region."
+  type        = string
+  default     = "us-east-2"
+}
+
+variable vsi5_vpc_subnet_name {
+  description = "Name of the subnet where the VSI will be deployed. Subnet MUST be part of the VPC and zone or else VSI creation will fail."
+  type        = string
+  default     = "subnet-az2-priv"
+}
+
+##############################################################################
+
+
+##############################################################################
+# VSI 6 Variables
+##############################################################################
+
+variable vsi6_name {
+  description = "Name of the VSI instance to be created"
+  type        = string
+  default     = "scylla-vsi-3"
+}
+
+variable vsi6_image_name {
+  description = "Image name used for VSI. Run 'ibmcloud is images' to find available images in a region"
+  type        = string
+  default     = "ibm-centos-8-3-minimal-amd64-3"
+}
+
+variable vsi6_machine_profile {
+  description = "VSI machine profile (i.e. mx2-32x256). Run 'ibmcloud is instance-profiles' to get a list of regional profiles"
+  type        =  string
+  default     = "mx2-32x256"
+}
+
+variable vsi6_vpc_zone {
+  description = "Name of the zone where the VSI will be deployed (i.e. us-south-1, us-south-2, us-east-1, etc). Zone should match VPC region."
+  type        = string
+  default     = "us-east-3"
+}
+
+variable vsi6_vpc_subnet_name {
+  description = "Name of the subnet where the VSI will be deployed. Subnet MUST be part of the VPC and zone or else VSI creation will fail."
+  type        = string
+  default     = "subnet-az3-priv"
+}
+
+##############################################################################
+
+
+
+
+
+
+
+
+
---
+++ vsi/vsi.tf
@@ -0,0 +1,225 @@
+
+
+##############################################################################
+# Lookup resource objects needed for creating VSI.
+##############################################################################
+
+# VPC where VSI will be created
+data ibm_is_vpc vpc {
+    name = var.vpc_name
+}
+
+# SSH key for creating VSI
+data ibm_is_ssh_key ssh_key {
+    name = var.ssh_key_name
+}
+
+
+##############################################################################
+# Provision VSI 1
+##############################################################################
+
+# Get the id of the subnet name
+locals {
+  vsi1_subnet_ids = [
+    for i in data.ibm_is_vpc.vpc.subnets: 
+        i.id if var.vsi1_vpc_subnet_name == i.name
+  ]
+}
+
+
+# Image to be used (i.e. centos, redhat, etc)
+data ibm_is_image vsi1_image {
+    name = var.vsi1_image_name
+}
+
+resource ibm_is_instance vsi1 {
+    name           = var.vsi1_name
+    resource_group = data.ibm_resource_group.resource_group.id
+    tags           = var.tags
+    image          = data.ibm_is_image.vsi1_image.id
+    profile        = var.vsi1_machine_profile
+    keys           = [data.ibm_is_ssh_key.ssh_key.id]
+    vpc            = data.ibm_is_vpc.vpc.id
+    zone           = var.vsi1_vpc_zone
+    primary_network_interface {
+        name    = "eth0"
+        subnet  = local.vsi1_subnet_ids[0]
+    }   
+}
+
+
+##############################################################################
+# Provision VSI 2
+##############################################################################
+
+# Get the id of the subnet name
+locals {
+  vsi2_subnet_ids = [
+    for i in data.ibm_is_vpc.vpc.subnets: 
+        i.id if var.vsi2_vpc_subnet_name == i.name
+  ]
+}
+
+
+# Image to be used (i.e. centos, redhat, etc)
+data ibm_is_image vsi2_image {
+    name = var.vsi2_image_name
+}
+
+resource ibm_is_instance vsi2 {
+    name           = var.vsi2_name
+    resource_group = data.ibm_resource_group.resource_group.id
+    tags           = var.tags
+    image          = data.ibm_is_image.vsi2_image.id
+    profile        = var.vsi2_machine_profile
+    keys           = [data.ibm_is_ssh_key.ssh_key.id]
+    vpc            = data.ibm_is_vpc.vpc.id
+    zone           = var.vsi2_vpc_zone
+    primary_network_interface {
+        name    = "eth0"
+        subnet  = local.vsi2_subnet_ids[0]
+    }   
+}
+
+
+##############################################################################
+# Provision VSI 3
+##############################################################################
+
+# Get the id of the subnet name
+locals {
+  vsi3_subnet_ids = [
+    for i in data.ibm_is_vpc.vpc.subnets: 
+        i.id if var.vsi3_vpc_subnet_name == i.name
+  ]
+}
+
+
+# Image to be used (i.e. centos, redhat, etc)
+data ibm_is_image vsi3_image {
+    name = var.vsi3_image_name
+}
+
+resource ibm_is_instance vsi3 {
+    name           = var.vsi3_name
+    resource_group = data.ibm_resource_group.resource_group.id
+    tags           = var.tags
+    image          = data.ibm_is_image.vsi3_image.id
+    profile        = var.vsi3_machine_profile
+    keys           = [data.ibm_is_ssh_key.ssh_key.id]
+    vpc            = data.ibm_is_vpc.vpc.id
+    zone           = var.vsi3_vpc_zone
+    primary_network_interface {
+        name    = "eth0"
+        subnet  = local.vsi3_subnet_ids[0]
+    }   
+}
+
+##############################################################################
+# Provision VSI 4
+##############################################################################
+
+# Get the id of the subnet name
+locals {
+  vsi4_subnet_ids = [
+    for i in data.ibm_is_vpc.vpc.subnets: 
+        i.id if var.vsi4_vpc_subnet_name == i.name
+  ]
+}
+
+
+# Image to be used (i.e. centos, redhat, etc)
+data ibm_is_image vsi4_image {
+    name = var.vsi4_image_name
+}
+
+resource ibm_is_instance vsi4 {
+    name           = var.vsi4_name
+    resource_group = data.ibm_resource_group.resource_group.id
+    tags           = var.tags
+    image          = data.ibm_is_image.vsi4_image.id
+    profile        = var.vsi4_machine_profile
+    keys           = [data.ibm_is_ssh_key.ssh_key.id]
+    vpc            = data.ibm_is_vpc.vpc.id
+    zone           = var.vsi4_vpc_zone
+    primary_network_interface {
+        name    = "eth0"
+        subnet  = local.vsi4_subnet_ids[0]
+    }   
+}
+
+
+##############################################################################
+# Provision VSI 5
+##############################################################################
+
+# Get the id of the subnet name
+locals {
+  vsi5_subnet_ids = [
+    for i in data.ibm_is_vpc.vpc.subnets: 
+        i.id if var.vsi5_vpc_subnet_name == i.name
+  ]
+}
+
+
+# Image to be used (i.e. centos, redhat, etc)
+data ibm_is_image vsi5_image {
+    name = var.vsi5_image_name
+}
+
+resource ibm_is_instance vsi5 {
+    name           = var.vsi5_name
+    resource_group = data.ibm_resource_group.resource_group.id
+    tags           = var.tags
+    image          = data.ibm_is_image.vsi5_image.id
+    profile        = var.vsi5_machine_profile
+    keys           = [data.ibm_is_ssh_key.ssh_key.id]
+    vpc            = data.ibm_is_vpc.vpc.id
+    zone           = var.vsi5_vpc_zone
+    primary_network_interface {
+        name    = "eth0"
+        subnet  = local.vsi5_subnet_ids[0]
+    }   
+}
+
+
+##############################################################################
+# Provision VSI 6
+##############################################################################
+
+# Get the id of the subnet name
+locals {
+  vsi6_subnet_ids = [
+    for i in data.ibm_is_vpc.vpc.subnets: 
+        i.id if var.vsi6_vpc_subnet_name == i.name
+  ]
+}
+
+# Image to be used (i.e. centos, redhat, etc)
+data ibm_is_image vsi6_image {
+    name = var.vsi6_image_name
+}
+
+resource ibm_is_instance vsi6 {
+    name           = var.vsi6_name
+    resource_group = data.ibm_resource_group.resource_group.id
+    tags           = var.tags
+    image          = data.ibm_is_image.vsi6_image.id
+    profile        = var.vsi6_machine_profile
+    keys           = [data.ibm_is_ssh_key.ssh_key.id]
+    vpc            = data.ibm_is_vpc.vpc.id
+    zone           = var.vsi6_vpc_zone
+    primary_network_interface {
+        name    = "eth0"
+        subnet  = local.vsi6_subnet_ids[0]
+    }   
+}
+
+
+
+
+
+
+
+
