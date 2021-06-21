e38ab224c5828647454b6058a5128bc2b8343d9f
Bryan Daniel <41975184+bryand2@users.noreply.github.com>
2021-06-21 02:18:20 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -16,7 +16,7 @@ variable resource_group {
 variable ibm_region {
     description = "IBM Cloud region where all resources will be deployed."
     type        = string
-    default     = "us-east"
+    default     = "us-south"
 }
 
 variable tags {
